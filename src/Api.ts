18fa725f94ddf2eed7e32742e4b129a3adcb1dea
osamudiamen <osas.azamegbe@gmail.com>
2020-12-20 15:19:12 +0100

    FEAT: Created API call function

--- src/Api.ts
+++ src/Api.ts
@@ -1,1 +1,15 @@
-export {}
+enum Difficulty {
+    EASY = "easy",
+    MEDIUM = "medium",
+    HARD = "hard"
+}
+
+const fetchQuizQuestions = async (amount: number, difficulty: Difficulty) => {
+    const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
+    const response = await fetch(endpoint);
+    const data = await response.json();
+    console.log(data);
+    return 10
+}
+
+export {fetchQuizQuestions, Difficulty};
