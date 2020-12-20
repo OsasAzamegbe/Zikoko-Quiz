00240c89186e3f6a87c43efe1f82bf4fae4ee407
osamudiamen <osas.azamegbe@gmail.com>
2020-12-20 14:16:41 +0100

    FEAT: created base function skeletons

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,32 @@
 import React from 'react';
 
+import Card from './components/Card';
+
+
 function App() {
+
+  const startTrivia = async () => {
+
+  };
+
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
+
+  };
+
+  const nextQuestion = () => {
+
+  };
+
   return (
     <div className="App">
-      QUIZ
+      <h1>Zikoko Quiz</h1>
+      <button className="start" onClick={startTrivia}>Start</button>
+      <p className="score">Score:</p>
+      <p>Loading Questions ... </p>
+      <Card/>
+      <button className="next" onClick={nextQuestion}>Next Question</button>
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
 
+type Props = {
+    question?: string;
+    answer?: string[];
+    callback?: any;
+    userAnswer?: string;
+    questionNumber?: number;
+    totalQuestions?: number;
+}
 
-const Card = () => {
+const Card: React.FC<Props> = ({
+    question, answer,
+    callback, userAnswer,
+    questionNumber, totalQuestions
+}) => {
+
     return (
         <div>
             Card
