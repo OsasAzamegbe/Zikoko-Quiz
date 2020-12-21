96b9c2bd5dd663014e523d7235ab496aa34ff83e
osamudiamen <osas.azamegbe@gmail.com>
2020-12-21 13:30:59 +0100

    FEAT: Created types for question object

--- src/Api.ts
+++ src/Api.ts
@@ -2,13 +2,24 @@ enum Difficulty {
     EASY = "easy",
     MEDIUM = "medium",
     HARD = "hard"
-}
+};
+
+type Question = {
+    category: string;
+    correct_answer: string;
+    difficulty: Difficulty;
+    incorrect_answers: string[];
+    type: string;
+};
+
+type QuestionState = Question & {answers: string[]};
 
 const fetchQuizQuestions = async (amount: number, difficulty: Difficulty) => {
     const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
     const response = await fetch(endpoint);
     const data = await response.json();
-    return data;    
+    return data.results;    
 }
 
+export type {QuestionState, Question};
 export {fetchQuizQuestions, Difficulty};
